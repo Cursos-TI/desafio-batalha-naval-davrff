63d42e83e6700506b20beecca766dde64dbb64ff
Davi Rodrigues Felix de Freitas <132140189+davrff@users.noreply.github.com>
2025-11-09 12:17:20 +0000

    verificando se os navios cabem no tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -23,13 +23,25 @@ int main() {
 
     // escolhendo a posicao de cada navio
 
-    // navio1 estara na horinzontal
+    // navio1 estara na horizontal
     int linha_navio1 = 2;
     int coluna_navio1 = 3;
 
     // navio2 estara na vertical
     int linha_navio2 = 5;
     int coluna_navio2 = 6;
+
+    // checa se o navio horizontal ultrapassa o limite a direita
+    if (coluna_navio1 > TAM_TABULEIRO - TAM_NAVIO) {
+        printf("❌ O navio horinzontal não cabe na coluna %d.\n", coluna_navio1);
+        return 1;
+    }
+
+    // checa se o navio vertical ultrapassa o limite inferior
+    if (linha_navio1 > TAM_TABULEIRO - TAM_NAVIO) {
+        printf("❌ O navio vertical não cabe na linha %d.\n", linha_navio2);
+        return 1;
+    }
 
     
 
